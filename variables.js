48d80d94099bef3b61cb7f7ff2d3bc818ab3c068
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-03 03:12:23 +0000

    javascripting work most of which was saved under variables due to recovery error

---
+++ variables.js
@@ -0,0 +1,99 @@
+//var example = 'some string';
+
+var someString = 'this is a string';
+//console.log(someString);
+
+// var example = 'example string';
+//example.length
+//console.log(example.length);
+
+var pizza = 'pizza is alright';
+pizza = pizza.replace('alright', 'wonderful');
+//console.log(pizza);
+
+var example = 123456789;
+//console.log(example);
+
+//var roundUp = 1.5;
+//Math.round(0.5);
+//var rounded = Math.round(roundUp);
+//console.log(rounded);
+
+var n = 128;
+n = n.toString();
+//console.log(n);
+
+/*var fruit = 'orange';
+fruit.length
+if (fruit.length > 5) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('The fruit name has five characters or less.');
+}*/
+
+/*var total = 0;
+var limit = 45;
+for (var i = 0; i < limit; i++) {
+  total += i;
+}
+//console.log(i);*/
+
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+//console.log(pizzaToppings);
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var filtered = numbers.filter(function evenNumbers (number) {
+  return number % 2 === 0;
+});
+//console.log(filtered);
+
+//var food = ['apple', 'pizza', 'pear'];
+//console.log(food[1]);
+
+var pets = ['cat', 'dog', 'rat'];
+for (var i =0; i < 3; i++) {
+  pets[i] = pets[i] + 's';
+}
+//console.log(pets);
+
+var pizza = {  
+  toppings: ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+};  
+//console.log(pizza);
+
+var food = {
+  types: 'only pizza'
+};
+//console.log(food['types']);
+
+function eat (food) {
+  return food + ' tasted really good.';
+}
+//console.log(eat(['bananas']));
+
+function math (firstArg, secondArg, thirdArg) {
+  return (secondArg*thirdArg)+firstArg;
+}
+//console.log(math(53, 61, 67));
+
+var a = 1, b = 2, c = 3;  
+       
+     (function firstFunction(){  
+         var b = 5, c = 6;  
+       
+         (function secondFunction(){  
+             var b = 8;
+             console.log("a: "+a+", b: "+b+", c: "+c);
+       
+             (function thirdFunction(){  
+                 var a = 7, c = 9;  
+       
+                 (function fourthFunction(){  
+                     var a = 1, c = 8;  
+       
+                 })();  
+             })();  
+         })();  
+     })();
